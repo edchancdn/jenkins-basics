0eb1a70725891a4187781dda2958ff332a2559ba
edchancdn <edchan.cdn@gmail.com>
2022-05-30 12:13:10 -0400

    added comments

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
 
+    /*
+    Jenkins configuration
+
+    Repository URL:
+    https://<access token>@github.com/<userName>/<repository>.git
+
+    Build step:
+    cd src
+    javac Main.java
+    java Main
+
+    Note: The build step will
+    1 Go to src directory
+    2 Compile Main.java and generate Main.class
+    3 Execute Main class
+    See print lines in Jenkins logs
+     */
+
     public static void main(String[] args) {
         System.out.println("Fist line from app");
         System.out.println("Second line from app");
